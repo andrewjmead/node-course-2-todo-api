d5e9b13f6168e78427c7f4f1d219cfc976d76527
Andrew Mead <andrewjosephmead@gmail.com>
2016-08-25 14:08:37 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
