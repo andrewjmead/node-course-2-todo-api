734949a750086997a6a8ba607683da1d4f6358c0
Andrew Mead <andrewjosephmead@gmail.com>
2016-08-29 12:53:43 -0400

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -49,6 +49,24 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
